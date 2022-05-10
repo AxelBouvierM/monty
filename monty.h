362896c496b7223f79786b98e53027e695f9195b
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-10 20:02:18 +0000

    prototypes and structs

--- monty.h
+++ monty.h
@@ -39,4 +39,6 @@ void _pall(stack_t **stack, __attribute__ ((unused)) unsigned int line_number);
 int get_op(FILE *file, instruction_t op[]);
 int is_digit(char *str);
 void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
 #endif
