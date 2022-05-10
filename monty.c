6b78d2212d99a6dcf47185d561265da2d57379ee
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-10 19:16:38 +0000

    Function main

---
+++ monty.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * main - Function that reads the file and executes the functions
+ * @argc: Numbers of elements
+ * @argv: Elements
+ * Return: 0 if succes
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+
+	instruction_t op[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{NULL, NULL}
+	};
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file <%s>\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	get_op(file, op);
+	fclose(file);
+	return (0);
+}
