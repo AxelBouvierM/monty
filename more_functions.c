c048becd657e3cfb13806a4f0dbdeb220b90b855
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-11 16:00:38 +0000

    fixing

--- more_functions.c
+++ more_functions.c
@@ -72,6 +72,7 @@ void _div(stack_t **stack, unsigned int line_number)
 	if ((*stack)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 	div = (*stack)->next->n / (*stack)->n;
 	_pop(stack, line_number);
