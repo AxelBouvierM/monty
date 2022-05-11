cf2dc05552fa8d1bc9281fbaa866299d8c19fcef
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-11 18:40:26 +0000

    prototypes and structs

--- monty.h
+++ monty.h
@@ -50,4 +50,6 @@ void _mul(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
 void _pchar(stack_t **stack, unsigned int line_number);
 void _pstr(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+stack_t *add_dnodeint_end(stack_t **stack, int n);
 #endif
